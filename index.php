e838f647dae315d0f0bf9cebeaa9a9179abf9b3c
smithDevIan <smitten126@gmail.com>
2022-03-07 15:25:43 +0300

    simba

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $textArray=explode('*', $text);
 $userResponse=trim(end($textArray));
 
 
-switch ($userResponse) {
+/*switch ($userResponse) {
     case '':
         $response  = "CON We invite you to fundraise by adopting a poll station.\nReply with:.\n";
         $response .= "1.Yes \n";
@@ -19,12 +19,6 @@ switch ($userResponse) {
     
     case '1':
         $response .= "CON Please specify the poling station to adopt: \n";
-        continue;
-        if ($userResponse != "") {
-            $response .= "CON Thank you, Reply with amount to contribute: \n";
-            $response .= "1. 100 Ksh \n";
-            $response .= "2. Other amount \n";
-        }
         break;
 
     case '2':
@@ -34,36 +28,29 @@ switch ($userResponse) {
     default:
         # code...
         break;
-}
-
-
-/*if ($text == "") {
+}*/
+if ($$userResponse == "") {
     // This is the first request. Note how we start the response with CON
     $response  = "CON We invite you to fundraise by adopting a poll station.\nReply with:.\n";
     $response .= "1.Yes \n";
     $response .= "2. No";
 
-} else if ($text == "1") {
-    // Business logic for first level response
-    $response .= "CON Please specify the poling station to adopt: \n";
-    
-} else if ($text == "2") {
+}else if ($userResponse == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response .= "END Thank you ";
 
-}else if (!empty($incomming_text) && $text!=null){
+} else if ($userResponse == "1") {
+    // Business logic for first level response
+    $response .= "CON Please specify the poling station to adopt: \n";
+    
+} else{
         
     $response .= "CON Thank you, Reply with amount to contribute: \n";
     $response .= "1. 100 Ksh \n";
     $response .= "2. Other amount \n";
 
-}else if ((!empty($incomming_text)||$text!=null) && $text=="1"){
-    
-    
-    $response .= "END Thank you";
-
-}*/
+}
 
 // Echo the response back to the API
 header('Content-type: text/plain');
