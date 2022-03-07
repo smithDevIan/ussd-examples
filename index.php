1fbaacd0815653f348fa0782ea58b3da760acc86
smithDevIan <smitten126@gmail.com>
2022-03-07 07:32:07 +0300

    2nd step

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($text == "") {
 } else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your phone number is ".$phoneNumber;
+    $response = "END Thank you ";
 
 } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
