909c8b220ab79419845b676f196d1b566b68af3b
smithDevIan <smitten126@gmail.com>
2022-03-07 15:27:08 +0300

    simba

--- index.php
+++ index.php
@@ -29,28 +29,33 @@ $userResponse=trim(end($textArray));
         # code...
         break;
 }*/
-if ($$userResponse == "") {
+if ($text == "") {
     // This is the first request. Note how we start the response with CON
     $response  = "CON We invite you to fundraise by adopting a poll station.\nReply with:.\n";
     $response .= "1.Yes \n";
     $response .= "2. No";
 
-}else if ($userResponse == "2") {
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response .= "CON Please specify the poling station to adopt: \n";
+    
+} else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response .= "END Thank you ";
 
-} else if ($userResponse == "1") {
-    // Business logic for first level response
-    $response .= "CON Please specify the poling station to adopt: \n";
-    
-} else{
+}else if (!empty($incomming_text) && $text!=null){
         
     $response .= "CON Thank you, Reply with amount to contribute: \n";
     $response .= "1. 100 Ksh \n";
     $response .= "2. Other amount \n";
 
-}
+}else if ((!empty($incomming_text)||$text!=null) && $text=="1"){
+    
+    
+    $response .= "END Thank you";
+
+}*/
 
 // Echo the response back to the API
 header('Content-type: text/plain');
