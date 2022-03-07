757a58eaf829f6480afec0b46ea388f5d204ae32
smithDevIan <smitten126@gmail.com>
2022-03-07 15:42:07 +0300

    simba

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = ltrim($_POST["phoneNumber"],'+');
-$text        = $_POST["text"];
+$userResponse        = $_POST["text"];
 
 $textArray=explode('*', $text);
-//$userResponse=trim(end($textArray));
+$userResponse=trim(end($textArray));
 
 
 /*switch ($userResponse) {
@@ -29,28 +29,28 @@ $textArray=explode('*', $text);
         # code...
         break;
 }*/
-if ($textArray == "") {
+if ($userResponse == "") {
     // This is the first request. Note how we start the response with CON
     $response  = "CON We invite you to fundraise by adopting a poll station.\nReply with:.\n";
     $response .= "1.Yes \n";
     $response .= "2. No";
 
-} else if ($textArray == "1") {
+} else if ($userResponse == "1") {
     // Business logic for first level response
     $response .= "CON Please specify the poling station to adopt: \n";
     
-} else if ($textArray == "2") {
+} else if ($userResponse == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response .= "END Thank you ";
 
-}else if ($textArray="1*1"){
+}else if ($userResponse="1*1"){
         
     $response .= "CON Thank you, Reply with amount to contribute: \n";
     $response .= "1. 100 Ksh \n";
     $response .= "2. Other amount \n";
 
-}else if ($textArray="1*1*1"){
+}else if ($userResponse="1*1*1"){
         
     $response .= "CON Thank you, for paying. \n";
 
