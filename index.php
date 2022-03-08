8fd7eecedbb26d3a3cdfea2c3691bf8a503acc6a
smithDevIan <smitten126@gmail.com>
2022-03-08 11:32:05 +0300

    all

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	//3. Explode the text to get the value of the latest interaction - think 1*1
 	$inputs = explode('*', $text);
 	
-    $input = count($inputs);
+    $input = inputs.count();
 	//4. Set the default level of the user
     $text = '';
     switch($input){
