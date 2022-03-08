703629645891343b2d727a392e36b033f086f0c3
smithDevIan <smitten126@gmail.com>
2022-03-08 12:41:39 +0300

    all

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	
 	//4. Set the default level of the user
     //$text = '';
-    switch($inputs){
+    switch($text){
         case 0: 
             $response = "CON We invite you to fundraise by adopting a poll station.\nReply with:\n1.Yes \n2. No";
             break;
