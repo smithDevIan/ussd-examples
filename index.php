6153a2f9fe3be6feda32fa817d290dc66173d277
smithDevIan <smitten126@gmail.com>
2022-03-08 12:55:43 +0300

    all

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             $text = "CON We invite you to fundraise by adopting a poll station.\nReply with:\n1.Yes \n2. No";
             break;
          case 1: 
-            $input = $inputs[inputs.count - 1];
+            $input = $inputs[count($inputs)- 1];
             if($input == "1"){
                 $text = "CON Please specify the poling station to adopt: \n";
             }else{
@@ -28,8 +28,8 @@
             $text = "CON Thank you, Reply with amount to contribute:\n1. KES 100 2. \nOther amount";
             break;
         case 3: 
-            $input = $inputs[inputs.count - 1];
-            if(input == 1){
+            $input = $inputs[count($inputs)- 1];
+            if($input == "1"){
                 $text = "END STK PUSH initiated. Please wait fpr the M-PESA pop up then enter your M-PESA PIN";
             }else{
                 $text = "CON Please enter amount (KES)";
