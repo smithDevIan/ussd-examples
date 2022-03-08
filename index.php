e4117759b54c081e33339cf7d45caafa9f5ec06c
smithDevIan <smitten126@gmail.com>
2022-03-08 12:28:17 +0300

    all

--- index.php
+++ index.php
@@ -9,10 +9,11 @@
 
 	//3. Explode the text to get the value of the latest interaction - think 1*1
 	$inputs = explode('*', $text);
-	
+	$countInputs = count($inputs);
+    $split = array_filter($countInputs);
 	//4. Set the default level of the user
     $text = '';
-    switch(sizeof($inputs)){
+    switch(split){
         case 0: 
             $text = "CON We invite you to fundraise by adopting a poll station.\nReply with:\n1.Yes \n2. No";
             break;
