b2d5e9c8668a930daac54539123e267695a3ea43
smithDevIan <smitten126@gmail.com>
2022-03-08 12:39:12 +0300

    add

--- index.php
+++ index.php
@@ -11,35 +11,35 @@
 	$inputs = explode('*', $text);
 	
 	//4. Set the default level of the user
-    $text = '';
-    switch($inputs.count()){
+    //$text = '';
+    switch($inputs){
         case 0: 
-            $text = "CON We invite you to fundraise by adopting a poll station.\nReply with:\n1.Yes \n2. No";
+            $response = "CON We invite you to fundraise by adopting a poll station.\nReply with:\n1.Yes \n2. No";
             break;
          case 1: 
             $input = $inputs[inputs.count - 1];
             if($input == "1"){
-                $text = "CON Please specify the poling station to adopt: \n";
+                $response = "CON Please specify the poling station to adopt: \n";
             }else{
-                $text="END Thank you";
+                $response="END Thank you";
             }
             break;
         case 2: 
-            $text = "CON Thank you, Reply with amount to contribute:\n1. KES 100 2. \nOther amount";
+            $response = "CON Thank you, Reply with amount to contribute:\n1. KES 100 2. \nOther amount";
             break;
         case 3: 
             $input = $inputs[inputs.count - 1];
             if(input == 1){
-                $text = "END STK PUSH initiated. Please wait fpr the M-PESA pop up then enter your M-PESA PIN";
+                $response = "END STK PUSH initiated. Please wait fpr the M-PESA pop up then enter your M-PESA PIN";
             }else{
-                $text = "CON Please enter amount (KES)";
+                $response = "CON Please enter amount (KES)";
             }
             break; 
         case 4: 
-            $text = "END STK PUSH initiated. Please wait fpr the M-PESA pop up then enter your M-PESA PIN";  
+            $response = "END STK PUSH initiated. Please wait fpr the M-PESA pop up then enter your M-PESA PIN";  
             break;            
     }
     // Echo the response back to the API
     header('Content-type: text/plain');
-    echo $text;
+    echo $response;
 ?>
