005e45cf562be4002c8c4ef13cb1f069a6ee3b19
smithDevIan <smitten126@gmail.com>
2022-03-08 16:50:40 +0300

    all

--- dbconnect.php
+++ dbconnect.php
@@ -1,26 +1,22 @@
 <?php 
 
 //Connection Credentials
-$servername = '127.0.0.1';
-$username = 'root';
-$password = "";
-$database = "ussd-sms";
-$dbport = 3306;
+$host = '127.0.0.1';
+$port = 3306;
+$dbname = 'ussd-sms';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
-
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $database, $dbport);
-
-    // Check connection
-    if ($conn->connect_error) {
-      echo "Error connecting";
-      die("Connection failed: " . $conn->connect_error);
-    }else{
-      echo "connected successfully";
-    }
-    
-
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try {
+    $db = new mysqli($host, $user, $pass, $dbname, $port);
+    $db->set_charset($charset);
+    $db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+} catch (\mysqli_sql_exception $e) {
+     throw new \mysqli_sql_exception($e->getMessage(), $e->getCode());
+}
+unset($host, $dbname, $user, $pass, $charset, $port);
 
 
 ?>
