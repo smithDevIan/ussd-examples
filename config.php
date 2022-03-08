8333d7bb7f330b74b2f17537a43acb9328af5cae
smithDevIan <smitten126@gmail.com>
2022-03-08 11:00:16 +0300

    all

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//This is the config.php file
-
-// Specify your login credentials
-$username   = "smithotieno85@gmail.com";
-$apikey     = "4305a85a25d279625d8def38ab767eebd6e89b04c14fd69163a3357a626a11ed";
-
-?>
--- utils.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Util {
-    static $introduction_message = "We invite you to fundraise by adopting a poll station.\nReply with:\n1.Yes \n2.No \n";
-}
-
-?>
